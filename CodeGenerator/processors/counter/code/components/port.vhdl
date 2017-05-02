41622f32a6908cfd3d94d0a92c053004eddb7e9e
Max Brand <max.brand@uni-ulm.de>
2017-05-02 12:08:12 +0200

    XSD überarbeitet, Comp-Gen ausgelagert, Bug-Fixes
    + Attribute x und y für Editoren hinzugefügt
    + : zu system geändert
    + stack und memory gelöscht, werden später durch custom und group
    ersetzt
    + control-Tag eingeführt inkl system.auto
    + generieren aller Komponenten in "ComponentFactory" ausgelagert
    + ID-Check eingebaut, Con-Check verbessert und gefixt

---
+++ CodeGenerator/processors/counter/code/components/port.vhdl
@@ -0,0 +1,32 @@
+-- Auto generated
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY port IS
+  GENERIC
+  (
+    g_wordSize : integer := 31
+  );
+  PORT
+  (
+    p_clk : in std_logic;
+    p_write : in std_logic;
+    p_input0 : in std_logic_vector(g_wordSize DOWNTO 0);
+    p_word : out std_logic_vector(g_wordSize DOWNTO 0)
+  );
+END port;
+
+ARCHITECTURE behavior OF port IS
+  SIGNAL s_input : std_logic_vector(g_wordSie DOWNTO 0;
+BEGIN
+  s_input <= p_input0;
+  -- Behavior
+  PROCESS (p_clk) BEGIN
+    IF rising_edge(p_clk) AND p_write = '1' THEN
+      p_out <= s_input;
+    ELSE
+      p_out <= p_out;
+    END IF;
+  END PROCESS;
+END behavior;
